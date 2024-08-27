6872ac612a0371aa8c966343d487322419c7bb77
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-08-27 13:16:06 +0500

    Initial Commit

---
+++ Animate-WeatherApp/Animate_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animate_WeatherAppApp.swift
+//  Animate-WeatherApp
+//
+//  Created by A'zamjon Abdumuxtorov on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Animate_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
